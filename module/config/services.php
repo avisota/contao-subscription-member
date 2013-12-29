c9a82bfaf25226048963de9c92f516bb806fd634
Tristan Lins <tristan.lins@bit3.de>
2013-12-29 17:37:57 +0100

    Extract member subscription from avisota/contao package.

---
+++ module/config/services.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Define subscription managers
+ */
+
+$container['avisota.subscription.member'] = $container->share(
+	function ($container) {
+		return new \Avisota\Contao\SubscriptionMember\Subscription\MemberSubscriptionManager();
+	}
+);
+
+$container['avisota.subscription.managers']->append('avisota.subscription.member');
