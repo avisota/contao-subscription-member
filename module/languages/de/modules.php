a66bfd6a7f9d30ea5e11792392f97102e35ce4b1
Avibot <avibot@avisota.org>
2014-05-11 04:03:18 +0200

    Auto-Update translations from transifex

---
+++ module/languages/de/modules.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-03-25T14:16:46+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['avisota-subscription-member']['0'] = 'Avisota - Bestellung für Mitglieder';
+
