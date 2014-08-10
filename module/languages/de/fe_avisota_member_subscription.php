a93ecc0a44dd7153ece5b7f2cf61787a203de638
Avibot <avibot@avisota.org>
2014-08-10 04:02:37 +0200

    Auto-Update translations from transifex

---
+++ module/languages/de/fe_avisota_member_subscription.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-08-07T04:02:29+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['allreadySubscribed']    = 'Vielen Dank, aber Sie sind bereits für diesen Newsletter angemeldet.';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['confirm']               = 'Abonnement bestätigen';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['notSubscribed']         = 'Sie sind für diesen Newsletter nicht angemeldet.';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['subscribe']             = 'Anmelden';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['subscribeConfirmation'] = 'Ihre Anmeldung wurde erfolgreich aktiviert.';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['subscribed']            = 'Vielen Dank, Sie sind nun angemeldet. Bitte sehen Sie in Ihr Postfach, um die Bestätigungs-Mail anzusehen.';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['unsubscribe']           = 'Abmelden';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['unsubscribe_direct']    = 'Abmelden';
+$GLOBALS['TL_LANG']['fe_avisota_member_subscription']['unsubscribed']          = 'Sie sind jetzt von unserem Newsletter abgemeldet.';
+
---
+++ module/languages/de/orm_avisota_recipient_source.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/avisota-contao/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-08-07T04:02:31+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['members']['0']                                   = 'Mitglieder';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['members']['1']                                   = 'Das in Contao integrierte Mitgliedermanagement benutzen.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersGroupFilter']['0']                        = 'Gruppenfilter';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersGroupFilter']['1']                        = 'Filtern Sie die Empfänger nach Zugehörigkeit zu Gruppen.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersGroupFilter_condition']['0']              = 'Bedingung';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersGroupFilter_conditions']['in']            = 'enthält';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersGroupFilter_conditions']['not in']        = 'enthält nicht';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersGroupFilter_group']['0']                  = 'Gruppe';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersManageSubscriptionPage']['0']             = 'Verwaltungsseite';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersManageSubscriptionPage']['1']             = 'Bitte wählen Sie die Seite aus, auf der das Abonnement verwaltet wird.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter']['0']                     = 'Werte-Filter';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter']['1']                     = 'Abonnenten nach Werten filtern.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparator']['0']          = 'Vergleichsoperator';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['empty']     = 'ist leer';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['eq']        = '==';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['gt']        = '>';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['gte']       = '>=';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['lt']        = '<';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['lte']       = '<=';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['neq']       = '!=';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_comparators']['not_empty'] = 'Ist nicht leer';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_property']['0']            = 'Spalte';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersPropertyFilter_value']['0']               = 'Wert';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersUnsubscribePage']['0']                    = 'Abmeldeseite';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersUnsubscribePage']['1']                    = 'Bitte wählen Sie die Seite aus, die für direkte Abbestellungen benutzt werden soll.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersUseGroupFilter']['0']                     = 'Nach Gruppen filtern';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersUseGroupFilter']['1']                     = 'Filtern Sie die Empfänger nach Zugehörigkeit zu Gruppen.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersUsePropertyFilter']['0']                  = 'Nach Eigenschaften filtern';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['membersUsePropertyFilter']['1']                  = 'Abonnenten nach Eigenschaften und Werten filtern.';
+$GLOBALS['TL_LANG']['orm_avisota_recipient_source']['members_legend']                                 = 'Mitgliedereinstellungen';
+
