cedafcdcd269caf25489fb21a6f4b2c138aaf7a1
Sven Baumann <baumann.sv@gmail.com>
2016-01-05 11:56:05 +0100

    optimize imports

--- src/Avisota/Contao/SubscriptionMember/EventsSubscriber.php
+++ src/Avisota/Contao/SubscriptionMember/EventsSubscriber.php
@@ -16,8 +16,6 @@
 namespace Avisota\Contao\SubscriptionMember;
 
 use Avisota\Contao\Core\DataContainer\OptionsBuilder;
-use Avisota\Contao\Entity\Recipient;
-use Avisota\Contao\Entity\Subscription;
 use Avisota\Contao\Subscription\Event\PrepareSubscriptionEvent;
 use Avisota\Contao\Subscription\Event\ResolveRecipientEvent;
 use Avisota\Contao\Subscription\Event\SubscriptionAwareEvent;
@@ -26,26 +24,12 @@ use Avisota\Contao\Subscription\SubscriptionManager;
 use Avisota\Contao\SubscriptionNotificationCenterBridge\Event\BuildTokensFromRecipientEvent;
 use Avisota\Contao\SubscriptionRecipient\Event\ExportRecipientPropertyEvent;
 use Avisota\Contao\SubscriptionRecipient\Event\MigrateRecipientEvent;
-use Contao\Doctrine\DBAL\DoctrineDbalEvents;
-use Contao\Doctrine\DBAL\Event\InitializeEventManager;
-use Contao\Doctrine\ORM\EntityAccessor;
-use Contao\Doctrine\ORM\EntityHelper;
 use ContaoCommunityAlliance\Contao\Bindings\ContaoEvents;
-use ContaoCommunityAlliance\Contao\Bindings\Events\Backend\AddToUrlEvent;
 use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\LoadDataContainerEvent;
-use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\RedirectEvent;
-use ContaoCommunityAlliance\Contao\Bindings\Events\Controller\ReloadEvent;
 use ContaoCommunityAlliance\Contao\Bindings\Events\System\LoadLanguageFileEvent;
-use ContaoCommunityAlliance\Contao\Events\CreateOptions\CreateOptionsEvent;
-use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\GetEditModeButtonsEvent;
-use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\GetPropertyOptionsEvent;
-use ContaoCommunityAlliance\DcGeneral\DcGeneralEvents;
 use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
-use ContaoCommunityAlliance\DcGeneral\Event\ActionEvent;
-use ContaoCommunityAlliance\DcGeneral\Factory\DcGeneralFactory;
 use MenAtWork\MultiColumnWizard\Event\GetOptionsEvent;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
